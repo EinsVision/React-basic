cbed14c338e96a5443146fab3b86dae077c61d6c
EinsVision <einsvision@gmail.com>
2021-10-23 19:00:42 +0900

    04. JSX

--- basic-react/src/App.js
+++ basic-react/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = 'Practice Basic React';
+  const style = {
+    backgroundColor: 'black',
+    color: 'tomato',
+    fontSize: 24, 
+    padding: '1rem',
+  }
   return (
     <div className="app">
+      <div>04.JSX</div>
+      <div style={style}>{name}</div>
       <Hello />
       <Hello />
       <Hello />
+      <div>05.Props</div>
     </div>
   );
 }
