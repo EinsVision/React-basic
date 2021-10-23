1b0162f65c8c3ad0b874151b80b6a4d008caf1e3
EinsVision <einsvision@gmail.com>
2021-10-23 18:23:29 +0900

    Installed React application

---
+++ basic-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div className="app">
+      <Hello />
+      <Hello />
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ basic-react/src/Hello.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Hello() {
+  return <div>Hello React!<button>Test Button</button></div>;
+}
+
+export default Hello;
